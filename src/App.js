dc85ca53b177ee971bfa666c7de106db508f780d
Alex Silva <alexbastiansilvafigueroa@gmail.com>
2020-10-19 16:11:53 -0300

    Se agrega slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Navigation } from './components/Navigation'
+import { Slider } from './components/slider'
+
 
 export const App = () => {
   return (
     <>
       <Navigation />
+      <Slider />
 
 
 
