2f19f02dc6f43536d2e4ac0a704f671d60cc3caf
Alex Silva <alexbastiansilvafigueroa@gmail.com>
2020-10-19 21:59:54 -0300

    Se soluciona error de sintaxis en componente slider.jsx

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 
 export const Slider = () => {
-<<<<<<< HEAD
   return (
     <>
       <Carousel>
@@ -46,51 +45,3 @@ export const Slider = () => {
     </>
   );
 };
-=======
-    return (
-        <>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className = "d-block w-100"
-                        src={require('../assets/img/dog.jpg')}
-                        // fluid
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../assets/img/dog.jpg')} //Agregar imagen
-                        // fluid
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={require('../assets/img/dog.jpg')} //Agregar imagen
-                        // fluid
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-            </Carousel>
-        </>
-    )
-}
->>>>>>> e482dfc4ab5df6fb297b5ac86a2f25dbc08e1e58
