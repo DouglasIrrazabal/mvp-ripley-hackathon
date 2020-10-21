d29986344f9b7f6d2746a4862715994d3b566a0b
Douglas <douglasirrazaba@gmail.com>
2020-10-21 09:49:35 -0300

    /docs agregado

---
+++ docs/precache-manifest.ff6940c2ea70880f41f6ef60e79d70cd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c02eb084c51bcd4a152f1e4e17c8e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4555e265851c54b0c754",
+    "url": "/static/css/main.309dd522.chunk.css"
+  },
+  {
+    "revision": "97f4db003602ebe31e9d",
+    "url": "/static/js/2.e8f3baab.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.e8f3baab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4555e265851c54b0c754",
+    "url": "/static/js/main.9d6aee8f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e026cb3b5b40032e5c89f769a7f5fbdf",
+    "url": "/static/media/iconoInsta.e026cb3b.svg"
+  },
+  {
+    "revision": "e83ba85e7660d45b07088ba80bb5ffb8",
+    "url": "/static/media/iconoLinkedIn.e83ba85e.svg"
+  },
+  {
+    "revision": "8e05ee3cb0a7c2eb324197faea4c3a1a",
+    "url": "/static/media/iconoMedium.8e05ee3c.svg"
+  },
+  {
+    "revision": "c2a925117a70218084d1022294178d78",
+    "url": "/static/media/logo.c2a92511.svg"
+  },
+  {
+    "revision": "67dc219b5c66955bb2d3d19690e67d0c",
+    "url": "/static/media/r-morita.67dc219b.svg"
+  },
+  {
+    "revision": "ee8cbd715fccb1452f64f50d83e30373",
+    "url": "/static/media/r-osito.ee8cbd71.svg"
+  },
+  {
+    "revision": "c22b2e69e12849aa568dac65a7488b0e",
+    "url": "/static/media/r-works.c22b2e69.svg"
+  },
+  {
+    "revision": "b31482c01356f5dd668cfb19bb4e0c4a",
+    "url": "/static/media/slider-1.b31482c0.jpg"
+  },
+  {
+    "revision": "6d07d8436b2b8e422a6cb6457ea825b6",
+    "url": "/static/media/slider-2.6d07d843.jpg"
+  },
+  {
+    "revision": "271a189e881f2740e16bee04602fd312",
+    "url": "/static/media/slider-3.271a189e.jpg"
+  }
+]);
